0cb4625b8bd0c43aadfce9bdf47f541d4f1d3bd0
papob.boonpat <boonpat.papob@gmail.com>
2021-10-03 20:44:24 +0700

    graphql query

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
+import { gql, useQuery } from "@apollo/client";
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      title
+      body
+      rating
+      id
+    }
+  }
+`;
 
 const Homepage = () => {
-  const { data, error, loading } = useFetch("http://localhost:1337/reviews");
+  const { loading, error, data } = useQuery(REVIEWS);
+
   if (loading) return <p>Loading......</p>;
   if (error) return <p>Error :(</p>;
   console.log(data);
   return (
     <div>
-      {data.map((review) => (
+      {data?.reviews?.map((review) => (
         <div key={review.id} className="review-card">
           <div className="rating">{review.rating}</div>
           <h2>{review.title}</h2>
--- src/pages/ReviewDetails.js
+++ src/pages/ReviewDetails.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { useParams } from "react-router";
-import useFetch from "../hooks/useFetch";
+import { gql, useQuery } from "@apollo/client";
+
+const REVIEW = gql`
+  query GetReview($id: ID!) {
+    review(id: $id) {
+      title
+      body
+      rating
+      id
+    }
+  }
+`;
 
 const ReviewDetails = () => {
   const { id } = useParams();
-  const { loading, data, error } = useFetch(
-    "http://localhost:1337/reviews/" + id
-  );
+
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { id: id },
+  });
+
   if (loading) return <p>Loading......</p>;
   if (error) return <p>Error :(</p>;
   console.log(data);
 
   return (
     <div className="review-card">
-      <div className="rating">{data.rating}</div>
-      <h2>{data.title}</h2>
+      <div className="rating">{data.review.rating}</div>
+      <h2>{data.review.title}</h2>
       <small>cosole list</small>
-      <p>{data.body}</p>
+      <p>{data.review.body}</p>
     </div>
   );
 };
